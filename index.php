37da955d5cb35f3eb5e96e7838782656ef680a06
alxz <34018121+alxz@users.noreply.github.com>
2020-01-26 18:57:05 -0500

    new file:   assets/maps/cave.json 	new file:   assets/maps/collision_test.json 	new file:   assets/maps/commando.json 	new file:   assets/maps/cybernoid.json 	new file:   assets/maps/depthsort.json 	new file:   assets/maps/desert.json 	new file:   assets/maps/dual_view.json 	new file:   assets/maps/features_test (1).json 	new file:   assets/maps/features_test.json 	new file:   assets/maps/features_test_rotated.json 	new file:   assets/maps/flipped-tiles.json 	new file:   assets/maps/multi-layer-multi-tileset.json 	new file:   assets/maps/multi-tileset.json 	new file:   assets/maps/multilayered-map.json 	new file:   assets/maps/multiple-layers.json 	new file:   assets/maps/ninja-tilemap.json 	new file:   assets/maps/phaser_tiles.json 	new file:   assets/maps/platform.json 	new file:   assets/maps/super_mario.json 	new file:   assets/maps/tile_collision_test.json 	new file:   assets/maps/tile_properties.json 	new file:   assets/tilemaps/catastrophi_level1.csv 	new file:   assets/tilemaps/catastrophi_level2.csv 	new file:   assets/tilemaps/catastrophi_level3.csv 	new file:   assets/tiles/catastrophi_tiles.png 	new file:   assets/tiles/catastrophi_tiles_16.png 	new file:   assets/tiles/commando.png 	new file:   assets/tiles/cybernoid.png 	new file:   assets/tiles/dirt.png 	new file:   assets/tiles/gridtiles.png 	new file:   assets/tiles/ground-tile.png 	new file:   assets/tiles/ground_1x1.png 	new file:   assets/tiles/kenney.png 	new file:   assets/tiles/kenny_ground_64x64.png 	new file:   assets/tiles/kenny_items_64x64.png 	new file:   assets/tiles/kenny_platformer.png 	new file:   assets/tiles/kenny_platformer_64x64.png 	new file:   assets/tiles/muddy-ground.png 	new file:   assets/tiles/phaser_tiles.png 	new file:   assets/tiles/platformer_tiles.png 	new file:   assets/tiles/sci-fi-tiles.png 	new file:   assets/tiles/super_mario.png 	new file:   assets/tiles/tiles.png 	new file:   assets/tiles/tiles2.png 	new file:   assets/tiles/tmw_desert_spacing.png 	new file:   assets/tiles/walls_1x2.png 	new file:   assets/tilesets/catastrophi_tiles_16_blue.png 	new file:   assets/tilesets/super-mario-tiles.png 	new file:   css/style.css 	new file:   css/style_old.css 	new file:   index.html 	new file:   index.php 	new file:   js/catastrophi.js 	new file:   js/dw_xhr.js 	new file:   js/index.js 	new file:   js/indexExample.js 	new file:   js/jquery-3.4.1.min.js

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+   <meta charset="UTF-8" />
+   <title>Tile Maps V1 -  Game</title>
+
+    <!-- <script src="js/phaser.min.js"></script> -->
+    <script src="//cdn.jsdelivr.net/npm/phaser@3.21.0/dist/phaser.js"></script>
+    <!-- <script src="//cdn.jsdelivr.net/npm/phaser@3.21.0/dist/phaser.min.js"></script> -->
+   <!-- <script src="//cdn.jsdelivr.net/npm/phaser@3.16.2/dist/phaser.js"></script> -->
+   <link rel="stylesheet" href="css/style.css">
+   <script src="js/jquery-3.4.1.min.js"></script>
+   <!-- <script src="js/index.js"></script> -->
+   <script src="js/catastrophi.js"></script>
+<!--    <script src="lib/text-encoder-lite.min.js"></script> -->
+<!--    <script src="lib/easytimer.min.js" type="text/javascript"></script> -->
+   <style type="text/css">
+       body {
+           margin: 0;
+       }
+   </style>
+</head>
+<body style="background-color: #333;">
+</body>
+</html>
+<!-- <?php
+echo "Hello";
+?> -->
